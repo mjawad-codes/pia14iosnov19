e632b470df8277c8f280bb6ad0e21d7a90cdd102
Muhammad Jawad <jawadbqa@gmail.com>
2025-11-19 09:46:45 +0100

    another text

--- pia14iosnov19/ContentView.swift
+++ pia14iosnov19/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Tjena")
+            Text("Mera text")
         }
         .padding()
     }
