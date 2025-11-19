262bd7b4122911fbf6f0be2da53f6956d6e05a63
Muhammad Jawad <jawadbqa@gmail.com>
2025-11-19 09:14:45 +0100

    Initial Commit

---
+++ pia14iosnov19/pia14iosnov19App.swift
@@ -0,0 +1,17 @@
+//
+//  pia14iosnov19App.swift
+//  pia14iosnov19
+//
+//  Created by Muhammad Jawad on 2025-11-19.
+//
+
+import SwiftUI
+
+@main
+struct pia14iosnov19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
