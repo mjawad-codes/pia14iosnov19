3dfd4570f967cc0ffdfca14e2d42c340ec1ecc30
Muhammad Jawad <jawadbqa@gmail.com>
2025-11-21 12:05:42 +0100

    Other text

--- pia14iosnov19/ContentView.swift
+++ pia14iosnov19/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Tjena")
             Text("Mera text")
+            Text("Tjena")
         }
         .padding()
     }
