1a094ae9dfddd79ff4c0781a91159bab9fc33b1d
Jawad <118392191+mjawad-codes@users.noreply.github.com>
2025-11-24 10:02:26 +0100

    Replace 'Tjena' with 'Banan' in ContentView

--- pia14iosnov19/ContentView.swift
+++ pia14iosnov19/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Tjena")
             Text("Hey Hey Hey")
-            Text("Tjena")
+            Text("Banan")
         }
         .padding()
     }
